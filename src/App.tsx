67f2d0aa7ac4918ddf4cb5e7b752316e5656a3c8
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-24 19:24:54 +0200

    added events state, get_events function; updated html

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,56 @@
-import './App.css';
-import React, { Fragment, ReactElement } from 'react';
-import Sample from './components/Sample';
+import './App.scss';
+import React, { Fragment, Key, ReactElement, useEffect, useState } from 'react';
+import TopBar from './components/TopBar';
+import EventItem from './components/EventItem';
+import ShoppingCart from './components/ShoppingCart';
+import {EventItemData} from "./types";
 
 
 export default function App(): ReactElement {
 
+    let [events, setEvents] = useState<EventItemData[]>([]);
+
+    /** function that fetches list of events from api */
+    const get_events: Function = async function(controller: AbortController): Promise<void> {
+
+        try {
+            const response: Response = await fetch("https://teclead-ventures.github.io/data/london-events.json", {signal: controller.signal});
+            const data = await response.json();
+
+            setEvents(data);
+        }
+
+        catch (error) {
+            console.log(error);
+        }
+
+        return;
+    }
+
+    useEffect(
+
+        // effect handler
+        () => {
+            const controller: AbortController = new AbortController();
+            get_events(controller);
+            return () => {controller.abort()};
+        },
+
+        // dependency array
+        []);
+
     let html: ReactElement =
         <Fragment>
-            <h1>test header</h1>
-            <h1>test header</h1>
-            <Sample />
+            <TopBar />
+            <main>
+                <section className='EventList'>
+                    <ul>
+                        {events.map((item: any, index: Key) => {return <EventItem key={index} {...item} />})}
+                    </ul>
+                </section>
+            </main>
+            <footer className='Footer'>footer</footer>
+            <ShoppingCart />
         </Fragment>
 
     return html;}
