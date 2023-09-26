7d0b0a026fee21b6c46f95b73c789ab39604f177
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-09-26 21:39:43 +0200

    updated event item content

--- src/components/EventItem.tsx
+++ src/components/EventItem.tsx
@@ -1,10 +1,49 @@
+// css imports
 import "./EventItem.scss"
+
+// react imports
 import { ReactElement } from "react";
+
+// material components
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+
+// material icons
+import AddCircle from "@mui/icons-material/AddCircle";
+
+// types
 import { EventItemData } from "../types";
 
 
 export default function EventItem(event: EventItemData): ReactElement {
 
-    let html: ReactElement = <li className={EventItem.name}>{event.title}</li>
+    let html: ReactElement =
+
+        <Card
+            sx={{
+                minWidth: 275,
+                my: 1 }}>
+
+            <CardContent sx={{paddingBottom: 1}} >
+
+                <Typography>{event.title}</Typography>
+                <Typography>{event.city.charAt(0).toLocaleUpperCase() + event.city.slice(1)}</Typography>
+                <Typography>{event.date.toString()}</Typography>
+
+                <Button
+                    variant="contained"
+                    endIcon={<AddCircle />}>
+
+                    Add to Cart
+                </Button>
+
+            </CardContent>
+
+        </Card>
 
     return html;}
